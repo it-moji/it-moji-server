823e8e0b8f6e5803415d5b2cef6a9dbaa8abace7
ryan-dia <cjfdnjs158@naver.com>
2025-02-09 09:54:51 +0900

    hotfix: adjust start page from 1 to 0 to fix pagination logic

--- src/main/java/com/itmoji/itmojiserver/api/v1/announcement/dto/request/PostSearchRequestDTO.java
+++ src/main/java/com/itmoji/itmojiserver/api/v1/announcement/dto/request/PostSearchRequestDTO.java
@@ -8,6 +8,7 @@ public record PostSearchRequestDTO(
 ) {
     private static final int MIN_PAGE = 1;
     private static final int REVISION_PAGE = 1;
+    private static final int START_PAGE = 0;
     private static final int DEFAULT_SIZE = 3;
 
     /**
@@ -16,7 +17,7 @@ public record PostSearchRequestDTO(
      * why? 프론트에서는 첫 페이지 기준이 1이라고 생각하고 모든 로직을 구성합니다.
      */
     public PostSearchRequestDTO {
-        page = (page == null || page < MIN_PAGE) ? MIN_PAGE : page - REVISION_PAGE;
+        page = (page == null || page < MIN_PAGE) ? START_PAGE : page - REVISION_PAGE;
         size = (size == null || size <= 0) ? DEFAULT_SIZE : size;
     }
 }
